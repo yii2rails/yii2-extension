3b49dbfd2b3a179b44554acd1ae0a32d22811cef
Vitaliy <theyamshikov@yandex.ru>
2019-02-05 23:55:15 +0600

    make enum

---
+++ src/common/enums/StatusEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace yii2rails\extension\common\enums;
+
+use yii2rails\extension\enum\base\BaseEnum;
+
+class StatusEnum extends BaseEnum {
+
+    const DISABLE = 0;
+    const ENABLE = 1;
+    const REJECTED = 2;
+
+}
