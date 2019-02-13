b8d8f80d6d940019b545bcd77eec6ddbfed33e46
Vitaliy <theyamshikov@yandex.ru>
2019-02-13 21:53:09 +0600

    clean

--- src/common/enums/StatusEnum.php
+++ src/common/enums/StatusEnum.php
@@ -6,8 +6,13 @@ use yii2rails\extension\enum\base\BaseEnum;
 
 class StatusEnum extends BaseEnum {
 
+    // откоючен
     const DISABLE = 0;
+
+    // включен
     const ENABLE = 1;
+
+    // отвергнут
     const REJECTED = 2;
 
 }
