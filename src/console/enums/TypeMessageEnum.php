b636451d60e23656cd4c5a1d1c95a5ecb86ed8c5
Vitaliy <theyamshikov@yandex.ru>
2019-02-27 18:04:31 +0600

    make enum

---
+++ src/console/enums/TypeMessageEnum.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace yii2rails\extension\console\enums;
+
+class TypeMessageEnum {
+	
+    const INFO = 'info';
+    const DANGER = 'danger';
+    const SUCCESS = 'success';
+    const WARNING = 'warning';
+    const PRIMARY = 'primary';
+    const DEFAULT = 'default';
+    const CUSTOM = 'custom';
+
+}
