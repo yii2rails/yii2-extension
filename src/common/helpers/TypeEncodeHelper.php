d9abee94c226dbffab8d11e4fb50bc7e844661d8
Vitaliy <theyamshikov@yandex.ru>
2019-06-14 22:56:42 +0600

    refacto(TypeEncodeHelper)

--- src/common/helpers/TypeEncodeHelper.php
+++ src/common/helpers/TypeEncodeHelper.php
@@ -2,15 +2,22 @@
 
 namespace yii2rails\extension\common\helpers;
 
+use yii\helpers\ArrayHelper;
+
 class TypeEncodeHelper {
 	
 	public static function bool($value) {
-        if($value == 'true' || $value == 'on') {
-            return true;
-        }
-        if($value == 'false' || $value == 'off') {
-            return false;
-        }
+	    $values = [
+	        'true' => true,
+	        'false' => false,
+	        'on' => true,
+	        'off' => false,
+	        'yes' => true,
+	        'no' => false,
+	        '1' => true,
+	        '0' => false,
+        ];
+        $value = ArrayHelper::getValue($values, $value, $value);
         return boolval($value);
     }
 	
